2b6c5cd9829da3cead9304a7f5cde11125e7d4ef
Ajay Singh <115453151+AjaySingh0812@users.noreply.github.com>
2024-12-31 12:17:03 +0000

    Datatypes

---
+++ Basics/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"// treat all js code as newer version
+
+//alert (3+3)//we are using nodejs , not browser
+
+console.log ("Ajay")
+
+let name = "Ajay"
+let age =22
+let isLoggedIn = false
+
+
+
+//number =>2 to power 53
+//bigint
+// string =>""
+//boolean => true/false
+//null
+// undefine =>
+//symbol => unique
+
+//object
+
+console.log(typeof null);//null ka type object hai
+
+console.log(typeof undefine)//undefine ka type undefine hi hai
