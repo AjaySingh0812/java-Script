d8c1f64514c011894336f354ed24f3304cf886f0
Ajay Singh <115453151+AjaySingh0812@users.noreply.github.com>
2024-12-31 12:15:24 +0000

    variable

---
+++ Basics/variable.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "ajaysingh99064@gmil.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountEmail = 2 //not allowed
+
+accountEmail = "aj@aj.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+/*
+prefer not to use var because of issue in block scope and functional scope.
+
+in other words in initial days js was not working on the basis of scope
+
+*/
+
+
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
